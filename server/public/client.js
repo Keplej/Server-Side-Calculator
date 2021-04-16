8192bea694b49212c44f25a760c3582d5c99e203
Johnny Keple <Johnny.Keple@gmail.com>
2021-04-16 06:09:23 -0500

    Got add button to show up in console

--- server/public/client.js
+++ server/public/client.js
@@ -6,5 +6,10 @@ $(document).ready(onReady);
 //This is where we will have out click functions at
 function onReady() {
     console.log('onReady function');
+    $('#add-button').on('click', adding);
 }
 
+function adding() {
+    console.log('we pressed adding');
+    operator = '+';
+}
